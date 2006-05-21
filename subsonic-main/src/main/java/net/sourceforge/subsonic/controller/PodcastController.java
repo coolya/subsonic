0e2659d40ef1c6fa778f0597129b4661af20780e
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2006-05-21 10:42:43 +0000

    Rewrite to Spring MVC.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@88 24148959-8e0e-0410-9c20-a6ce11f09cd5

---
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/controller/PodcastController.java
@@ -0,0 +1,100 @@
+package net.sourceforge.subsonic.controller;
+
+import org.springframework.web.servlet.mvc.*;
+import org.springframework.web.servlet.*;
+import net.sourceforge.subsonic.service.*;
+import net.sourceforge.subsonic.domain.*;
+import net.sourceforge.subsonic.util.*;
+
+import javax.servlet.http.*;
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+/**
+ * Controller for the page used to generate the Podcast XML file.
+ *
+ * @author Sindre Mehus
+ */
+public class PodcastController extends ParameterizableViewController {
+
+    private static final DateFormat RSS_DATE_FORMAT = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.US);
+    private PlaylistService playlistService;
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String url = request.getRequestURL().toString();
+        File[] playlists = playlistService.getSavedPlaylists();
+        List<Podcast> podcasts = new ArrayList<Podcast>();
+
+        for (int i = 0; i < playlists.length; i++) {
+
+            String name = StringUtil.removeSuffix(playlists[i].getName());
+            String encodedName = StringUtil.urlEncode(playlists[i].getName());
+            String publishDate = RSS_DATE_FORMAT.format(new Date(playlists[i].lastModified()));
+
+            // Resolve content type.
+            Playlist playlist = new Playlist();
+            playlistService.loadPlaylist(playlist, playlists[i].getName());
+            String suffix = playlist.getSuffix();
+            String type = StringUtil.getMimeType(suffix);
+
+            long length = playlist.length();
+            String enclosureUrl = url.replaceFirst("/podcast.*", "/stream?playlist=" + encodedName + "&amp;suffix=" + suffix);
+
+            podcasts.add(new Podcast(name, publishDate, enclosureUrl, length, type));
+        }
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        ModelAndView result = super.handleRequestInternal(request, response);
+        map.put("url", url);
+        map.put("podcasts", podcasts);
+
+        result.addObject("model", map);
+        return result;
+    }
+
+    public void setPlaylistService(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
+
+    /**
+     * Contains information about a single Podcast.
+     */
+    public static class Podcast {
+        private String name;
+        private String publishDate;
+        private String enclosureUrl;
+        private long length;
+        private String type;
+
+        public Podcast(String name, String publishDate, String enclosureUrl, long length, String type) {
+            this.name = name;
+            this.publishDate = publishDate;
+            this.enclosureUrl = enclosureUrl;
+            this.length = length;
+            this.type = type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPublishDate() {
+            return publishDate;
+        }
+
+        public String getEnclosureUrl() {
+            return enclosureUrl;
+        }
+
+        public long getLength() {
+            return length;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+}
