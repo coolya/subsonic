0f9b6d7c7d75d411f28b671a4c25f6685b1d76b2
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2007-08-28 19:34:34 +0000

    Jetty integration.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@384 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-booter/src/main/java/net/sourceforge/subsonic/booter/Main.java
+++ subsonic-booter/src/main/java/net/sourceforge/subsonic/booter/Main.java
@@ -1,28 +1,51 @@
 package net.sourceforge.subsonic.booter;
 
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.webapp.WebAppContext;
+
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
 
 /**
  * @author Sindre Mehus
  */
 public class Main {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Subsonic");
 
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+    public Main() throws Exception {
 
-        String dir = System.getProperty("user.dir");
-        frame.getContentPane().add(new JLabel(dir));
-        frame.setBounds(300, 300, 300, 200);
-        frame.setVisible(true);
+        Server server = new Server();
+        SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setPort(8080);
+        server.addConnector(connector);
 
+        WebAppContext context = new WebAppContext();
+//        context.setTempDirectory(new File("/tmp/jetty"));
+        context.setContextPath("/subsonic");
+        context.setWar("file:///projects/subsonic/trunk/subsonic-main/target/subsonic.war");
 
+        server.addHandler(context);
+        server.start();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main();
+//        JFrame frame = new JFrame("Subsonic");
+//
+//        frame.addWindowListener(new WindowAdapter() {
+//            public void windowClosing(WindowEvent e) {
+//                System.exit(0);
+//            }
+//        });
+//
+//        String dir = System.getProperty("user.dir");
+//        frame.getContentPane().add(new JLabel(dir));
+//        frame.setBounds(300, 300, 300, 200);
+//        frame.setVisible(true);
     }
 }
