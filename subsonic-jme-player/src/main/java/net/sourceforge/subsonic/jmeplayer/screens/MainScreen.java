9f5683f3a56aa22903c70c140c1c3f16e171464b
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2008-06-12 14:16:54 +0000

    Added player test.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@601 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-jme-player/src/main/java/net/sourceforge/subsonic/jmeplayer/screens/MainScreen.java
+++ subsonic-jme-player/src/main/java/net/sourceforge/subsonic/jmeplayer/screens/MainScreen.java
@@ -1,5 +1,7 @@
 package net.sourceforge.subsonic.jmeplayer.screens;
 
+import net.sourceforge.subsonic.jmeplayer.Log;
+import net.sourceforge.subsonic.jmeplayer.LogFactory;
 import net.sourceforge.subsonic.jmeplayer.SubsonicMIDlet;
 import net.sourceforge.subsonic.jmeplayer.domain.MusicDirectory;
 import net.sourceforge.subsonic.jmeplayer.player.DownloadController;
@@ -11,6 +13,9 @@ import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.List;
+import javax.microedition.media.Manager;
+import javax.microedition.media.Player;
+import javax.microedition.media.PlayerListener;
 
 /**
  * Main screen.
@@ -18,6 +23,8 @@ import javax.microedition.lcdui.List;
  * @author Sindre Mehus
  */
 public class MainScreen extends List {
+
+    private static final Log LOG = LogFactory.create("MainScreen");
 
     private final Display display;
     private IndexScreen indexScreen;
@@ -30,6 +37,7 @@ public class MainScreen extends List {
 
         append("Music", null);
         append("Download", null);
+        append("Play", null);
         append("Settings", null);
         append("Exit", null);
 
@@ -51,9 +59,18 @@ public class MainScreen extends List {
                             download();
                             break;
                         case 2:
-                            settings();
+                            new Thread() {
+                                public void run() {
+                                    LOG.debug("Play thread started: " + this);
+                                    play();
+                                    LOG.debug("Play thread done: " + this);
+                                }
+                            }.start();
                             break;
                         case 3:
+                            settings();
+                            break;
+                        case 4:
                             midlet.exit();
                             break;
                         default:
@@ -81,6 +98,34 @@ public class MainScreen extends List {
         });
 
         downloadController.download(new MusicDirectory.Entry("Real Life", null, false, "http://localhost/subsonic/stream?player=2&pathUtf8Hex=653a5c6d757369635c5468652053657074656d626572205768656e5c487567676572204d75676765725c3130202d205265616c204c6966652e6d7033&suffix=.mp3", null));
+    }
+
+    private void play() {
+        // TODO: Testing
+        Player player = null;
+        try {
+//            String url = "http://sindre.dyndns.org:8080/stream?player=2&pathUtf8Hex=653a5c6d757369635c5468652053657074656d626572205768656e5c487567676572204d75676765725c3130202d205265616c204c6966652e6d7033&suffix=.mp3";
+            String url = "http://sindre.dyndns.org:8080/stream?player=8&pathUtf8Hex=633a5c6d757369635c522e452e4d5c477265656e5c30382e207475726e20796f7520696e736964652d6f75742e6d7033&suffix=.mp3";
+            LOG.debug("Creating player for " + url);
+            player = Manager.createPlayer(url);
+            LOG.debug("Player created.");
+            player.addPlayerListener(new PlayerListener() {
+                public void playerUpdate(Player player, String event, Object eventData) {
+                    LOG.debug("playerUpdate(" + event + ")");
+                }
+            });
+            player.realize();
+            LOG.debug("Player realized.");
+            LOG.debug("Duration: " + player.getDuration());
+            player.start();
+            LOG.debug("Player started.");
+        } catch (Throwable x) {
+            LOG.error("Error.", x);
+        } finally {
+            if (player != null) {
+                player.close();
+            }
+        }
     }
 
     private void settings() {
