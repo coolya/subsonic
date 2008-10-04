4a0a7d3e314da65287891817aab48a2065f32afa
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2008-10-04 17:16:53 +0000

    New web.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@684 24148959-8e0e-0410-9c20-a6ce11f09cd5

---
+++ subsonic-web/banner.php
@@ -0,0 +1,35 @@
+<div id="banner-mini">
+    <div id="newversion"><a href="/download/"><img src="inc/img/newversion.png" alt="New Version 3"/></a></div>
+    <div id="bannercontent">
+        <div class="slide1 slide">
+            <div class="slidecontent">
+                <img src="inc/img/slides/minicovers.jpg" alt="CD Covers" class="image"/>
+
+                <div class="text">Organize 50,000+ tracks, whether they're Rock, Classical, Podcasts, or Audiobooks!
+                </div>
+            </div>
+        </div>
+        <div class="slide2 slide">
+            <div class="slidecontent">
+                <img src="inc/img/slides/miniplayers.jpg" alt="MP3 Players" class="image"/>
+
+                <div class="text">Synch your iPod, iPhone, or other device.</div>
+            </div>
+        </div>
+        <div class="slide3 slide">
+            <div class="slidecontent">
+                <img src="inc/img/slides/miniwave.gif" alt="Lossless Music" class="image"/>
+
+                <div class="text">Convert and Level lossless music on the fly.</div>
+            </div>
+        </div>
+        <div class="slide4 slide">
+            <div class="slidecontent">
+                <img src="inc/img/slides/minilogo.gif" alt="MediaMonkey" class="image"/>
+
+                <div class="text">Get MediaMonkey: The Music Manager for Serious Collectors</div>
+                <a href="/download/" class="downloadnow"><img src="inc/img/slides/downloadnow.gif" alt="Download Now"/></a>
+            </div>
+        </div>
+    </div>
+</div>
--- subsonic-web/menu.php
+++ subsonic-web/menu.php
@@ -1,4 +1,4 @@
-<div id="logo"><a href="index.php"><img src="inc/img/subsonic-inverse.png" alt="Subsonic"/></a></div>
+<div id="logo"><a href="index.php"><img src="inc/img/subsonic.png" alt="Subsonic"/></a></div>
 
 <div class="hide">
 </div>
@@ -17,78 +17,5 @@
     </ul>
 </div>
 <hr/>
-<div id="banner-full">
-    <div id="bannernavigation">
-        <div id="banner-prev">Previous</div>
-        <div id="banner-next">Next</div>
-    </div>
-    <div id="newversion"><a href="/download/"><img src="inc/img/newversion.png" alt="New Version 3"/></a></div>
-    <div id="bannercontent">
-        <div class="slide1 slide">
-            <div class="slidecontent">
-                <img src="inc/img/slides/messycds.jpg" alt="Screenshot" class="screenshot"/>
-                <img src="inc/img/slides/slogan.gif" title="MediaMonkey: Get your music collection in order!"
-                     alt="MediaMonkey: Get your music collection in order!" class="title"/>
-                <a href="/download/" class="downloadnow"><img src="inc/img/slides/downloadnow.gif" alt="Download Now"/></a>
-                <a href="/download/" class="getgold"><img src="inc/img/slides/getgold.gif" alt="Get Gold"/></a>
 
-                <div class="text">Is your music library a mess? <br/> Missing Album Art and other information? <br/>
-                    Scattered across various locations? <br/> Full of duplicates? <br/><em><strong>Get MediaMonkey and
-                    get organized.</strong></em></div>
-            </div>
-        </div>
-        <div class="slide2 slide">
-            <div class="slidecontent">
-                <img src="inc/img/slides/organise.jpg" alt="Screenshot" class="screenshot"/>
-
-                <div class="title">
-                    <div class="large">Organize</div>
-                    <div class="small">MediaMonkey: The Music Organizer for Serious Collectors</div>
-                </div>
-                <div class="text">Manage small to very large collections of audio files and playlists (50,000+), whether
-                    on a hard drive, network, or CDs. Lookup missing information and album art online, tag almost any
-                    audio format, and automatically rename/re-organize tracks on your hard drive.
-                </div>
-            </div>
-        </div>
-        <div class="slide3 slide">
-            <div class="slidecontent">
-                <img src="inc/img/slides/touchsync.jpg" alt="Screenshot" class="screenshot"/>
-
-                <div class="title">
-                    <div class="large">Sync</div>
-                    <div class="small">MediaMonkey: The Universal Music Manager</div>
-                </div>
-                <div class="text">Sync music and playlists to your iPod, iPhone, Zen, or most any other device, and let
-                    MediaMonkey convert unsupported formats and normalize volume levels on the fly.
-                </div>
-            </div>
-        </div>
-        <div class="slide4 slide">
-            <div class="slidecontent">
-                <img src="inc/img/slides/play.gif" alt="Screenshot" class="screenshot"/>
-
-                <div class="title">
-                    <div class="large">Play</div>
-                    <div class="small">MediaMonkey: The Organizer for Music Lovers</div>
-                </div>
-                <div class="text">Rip Music, Download Podcasts, and scan your drive for tracks. Let MediaMonkey
-                    automatically create a playlist and level volume as it plays.
-                </div>
-            </div>
-        </div>
-        <div class="slide5 slide">
-            <div class="slidecontent">
-                <img src="inc/img/slides/customize.jpg" alt="Screenshot" class="screenshot"/>
-
-                <div class="title">
-                    <div class="large">Customize</div>
-                    <div class="small">MediaMonkey: For Uncompromising Collectors</div>
-                </div>
-                <div class="text">Customize with Skins, plugins, visualizations, and hundreds of user-written scripts
-                    that extend MediaMonkey's functionality.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<?php include("banner.php"); ?>
