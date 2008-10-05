faa819fe3b6d3001292366aef10378f5a4ed12c6
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2008-10-05 19:51:10 +0000

    New web.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@686 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-web/menu.php
+++ subsonic-web/menu.php
@@ -11,7 +11,7 @@
         <li id="menu-screenshots"><a href="screenshots.php" class="<?php if ($current == 'screenshots') echo('open');?>"><span>Screenshots</span></a></li>
         <li id="menu-download"><a href="download.php" class="<?php if ($current == 'download') echo('open');?>"><span>Download</span></a></li>
         <li id="menu-installation"><a href="installation.php" class="<?php if ($current == 'installation') echo('open');?>"><span>Installation</span></a></li>
-        <!--<li id="menu-transcoding"><a href="transcoding.php" class="<?php if ($current == 'transcoding') echo('open');?>"><span>Transcoding</span></a></li>-->
+        <li id="menu-transcoding"><a href="transcoding.php" class="<?php if ($current == 'transcoding') echo('open');?>"><span>Transcoding</span></a></li>
         <li id="menu-changelog"><a href="changelog.php" class="<?php if ($current == 'changelog') echo('open');?>"><span>Change Log</span></a></li>
         <li id="menu-forum"><a href="forum.php" class="<?php if ($current == 'forum') echo('open');?>"><span>Forum</span></a></li>
     </ul>
