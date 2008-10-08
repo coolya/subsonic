c1738f6522db913b37998e2532a00227d9d94647
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2008-10-08 06:46:59 +0000

    New web.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@692 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-web/footer.php
+++ subsonic-web/footer.php
@@ -1,6 +1,4 @@
 <div id="footer">
-    <!--<a href="http://www.cleverstarfish.com/" id="starfish"><img src="inc/img/cleverstarfish.gif" alt="Site by Clever Starfish" /></a>-->
-    <!--<a href="http://sourceforge.net/projects/subsonic/" id="starfish"><img src="http://sourceforge.net/sflogo.php?group_id=126265&type=1" alt="SourceForge.net"/></a>-->
     <div class="footercontent">
         <div>&copy; Copyright 2008 Sindre Mehus</div>
     </div>
