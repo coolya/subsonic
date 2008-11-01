385b10bec1755270e3331a78f49b5627b2350612
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2008-11-01 11:21:35 +0000

    Provide direct logon link if admin username is not changed. Create auto-login option in MultiController.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@710 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-main/src/main/java/net/sourceforge/subsonic/controller/MultiController.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/controller/MultiController.java
@@ -1,14 +1,17 @@
 package net.sourceforge.subsonic.controller;
 
 import org.springframework.web.servlet.*;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.servlet.mvc.multiaction.*;
 
 import javax.servlet.http.*;
 import java.util.*;
+import java.net.URLEncoder;
 
 import net.sourceforge.subsonic.service.SecurityService;
 import net.sourceforge.subsonic.service.SettingsService;
 import net.sourceforge.subsonic.domain.User;
+import net.sourceforge.subsonic.util.StringUtil;
 
 /**
  * Multi-controller used for simple pages.
@@ -20,7 +23,18 @@ public class MultiController extends MultiActionController {
     private SecurityService securityService;
     private SettingsService settingsService;
 
-    public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        // Auto-login if "user" and "password" parameters are given.
+        String username = request.getParameter("user");
+        String password = request.getParameter("password");
+        if (username != null && password != null) {
+            username = URLEncoder.encode(username, StringUtil.ENCODING_UTF8);
+            password = URLEncoder.encode(password, StringUtil.ENCODING_UTF8);
+            return new ModelAndView(new RedirectView("j_acegi_security_check?j_username=" + username +
+                                                     "&j_password=" + password + "&_acegi_security_remember_me=checked"));
+        }
+
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("logout", request.getParameter("logout") != null);
         map.put("error", request.getParameter("error") != null);
