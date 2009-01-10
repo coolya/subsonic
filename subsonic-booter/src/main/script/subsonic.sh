dc0980f291c7304964f1bc8a493126a3de022dcd
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2009-01-10 15:20:38 +0000

    Use JAVA_HOME if set in subsonic.sh
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@786 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-booter/src/main/script/subsonic.sh
+++ subsonic-booter/src/main/script/subsonic.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+###################################################################################
+#
+# Shell script for starting Subsonic.  See http://subsonic.sourceforge.net.
+#
+# Normally, you only need to modify the following four variables.
+#
+###################################################################################
+
 # The directory where Subsonic will create files. Make sure it is writable.
 SUBSONIC_HOME=/var/subsonic
 
@@ -12,15 +20,27 @@ SUBSONIC_CONTEXT_PATH=/
 # The memory limit (max Java heap size) in megabytes.
 MAX_MEMORY=64
 
+###################################################################################
+
 
 echo Starting Subsonic...
-cd `dirname $0`
 
+# Use JAVA_HOME if set, otherwise assume java is in the path.
+JAVA=java
+if [ -e "${JAVA_HOME}" ]
+    then
+    JAVA=${JAVA_HOME}/bin/java
+fi
+
+# Create Subsonic home directory.
 mkdir -p ${SUBSONIC_HOME}
 LOG=${SUBSONIC_HOME}/subsonic_sh.log
 rm -f ${LOG}
 
-java -Xmx${MAX_MEMORY}m  -Dsubsonic.home=${SUBSONIC_HOME} -Dsubsonic.port=${SUBSONIC_PORT} \
+cd `dirname $0`
+
+
+${JAVA} -Xmx${MAX_MEMORY}m  -Dsubsonic.home=${SUBSONIC_HOME} -Dsubsonic.port=${SUBSONIC_PORT} \
 -Dsubsonic.contextPath=${SUBSONIC_CONTEXT_PATH} -jar subsonic-booter-jar-with-dependencies.jar > ${LOG} 2>&1 &
 
 echo Started Subsonic. See log file ${LOG}
