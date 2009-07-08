0651a54c1685f66218aa4cf0f7b84e4f3ae9a59b
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2009-07-08 16:27:04 +0000

    Initial android module.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@923 24148959-8e0e-0410-9c20-a6ce11f09cd5

---
+++ subsonic-android/src/net/sourceforge/subsonic/android/SubsonicActivity.java
@@ -0,0 +1,15 @@
+package net.sourceforge.subsonic.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubsonicActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
