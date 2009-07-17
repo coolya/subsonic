7fc0c88b24f0e99db26085b2e3bb067d94005adb
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2009-07-17 12:06:11 +0000

    Android prototyping.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk/subsonic-android@948 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- src/net/sourceforge/subsonic/android/SelectAlbumActivity.java
+++ src/net/sourceforge/subsonic/android/SelectAlbumActivity.java
@@ -1,5 +1,8 @@
 package net.sourceforge.subsonic.android;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -8,26 +11,26 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
+import android.widget.TextView;
 import net.sourceforge.subsonic.android.domain.MusicDirectory;
 import net.sourceforge.subsonic.android.service.MusicService;
 import net.sourceforge.subsonic.android.service.MusicServiceFactory;
 import net.sourceforge.subsonic.android.util.BackgroundTask;
-
-import java.util.List;
-import java.util.ArrayList;
 
 public class SelectAlbumActivity extends Activity implements AdapterView.OnItemClickListener {
 
     private static final String TAG = SelectAlbumActivity.class.getSimpleName();
     private final DownloadServiceConnection downloadServiceConnection = new DownloadServiceConnection();
     private DownloadService downloadService;
-    private ListView albumList;
-    private ListView songList;
+    private ListView entryList;
     private Button downloadButton;
     private Button selectAllButton;
     private Button selectNoneButton;
@@ -44,13 +47,10 @@ public class SelectAlbumActivity extends Activity implements AdapterView.OnItemC
         downloadButton = (Button) findViewById(R.id.select_album_download);
         selectAllButton = (Button) findViewById(R.id.select_album_selectall);
         selectNoneButton = (Button) findViewById(R.id.select_album_selectnone);
-        albumList = (ListView) findViewById(R.id.select_album_albums);
-        songList = (ListView) findViewById(R.id.select_album_songs);
+        entryList = (ListView) findViewById(R.id.select_album_entries);
 
-        albumList.setOnItemClickListener(this);
-        songList.setItemsCanFocus(false);
-        songList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);// TODO:Specify in XML.
-        songList.setOnItemClickListener(this);
+        entryList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);// TODO:Specify in XML.
+        entryList.setOnItemClickListener(this);
 
         selectAllButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,10 +84,8 @@ public class SelectAlbumActivity extends Activity implements AdapterView.OnItemC
 
             @Override
             protected void done(MusicDirectory result) {
-                List<MusicDirectory.Entry> albums = result.getChildren(true, false);
-                List<MusicDirectory.Entry> songs = result.getChildren(false, true);
-                albumList.setAdapter(new ArrayAdapter<MusicDirectory.Entry>(SelectAlbumActivity.this, android.R.layout.simple_list_item_1, albums));
-                songList.setAdapter(new ArrayAdapter<MusicDirectory.Entry>(SelectAlbumActivity.this, android.R.layout.simple_list_item_multiple_choice, songs));
+                List<MusicDirectory.Entry> entries = result.getChildren();
+                entryList.setAdapter(new EntryAdapter(entries));
             }
 
             @Override
@@ -100,9 +98,9 @@ public class SelectAlbumActivity extends Activity implements AdapterView.OnItemC
     }
 
     private void selectAll(boolean selected) {
-        int count = songList.getCount();
+        int count = entryList.getCount();
         for (int i = 0; i < count; i++) {
-            songList.setItemChecked(i, selected);
+            entryList.setItemChecked(i, selected);
         }
     }
 
@@ -117,17 +115,16 @@ public class SelectAlbumActivity extends Activity implements AdapterView.OnItemC
         if (position >= 0) {
             MusicDirectory.Entry entry = (MusicDirectory.Entry) parent.getItemAtPosition(position);
             Log.d(TAG, entry + " clicked.");
-            // TODO: Use (view == albumList) instead?
             if (entry.isDirectory()) {
                 Intent intent = new Intent(this, SelectAlbumActivity.class);
                 intent.putExtra(Constants.NAME_PATH, entry.getId());
                 intent.putExtra(Constants.NAME_NAME, entry.getName());
                 startActivity(intent);
             } else {
-                int count = songList.getCount();
+                int count = entryList.getCount();
                 boolean checked = false;
                 for (int i = 0; i < count; i++) {
-                    if (songList.isItemChecked(i)) {
+                    if (entryList.isItemChecked(i)) {
                         checked = true;
                         break;
                     }
@@ -141,10 +138,10 @@ public class SelectAlbumActivity extends Activity implements AdapterView.OnItemC
         try {
             if (downloadService != null) {
                 List<MusicDirectory.Entry> songs = new ArrayList<MusicDirectory.Entry>(10);
-                int count = songList.getCount();
+                int count = entryList.getCount();
                 for (int i = 0; i < count; i++) {
-                    if (songList.isItemChecked(i)) {
-                        songs.add((MusicDirectory.Entry) songList.getItemAtPosition(i));
+                    if (entryList.isItemChecked(i)) {
+                        songs.add((MusicDirectory.Entry) entryList.getItemAtPosition(i));
                     }
                 }
                 downloadService.download(songs);
@@ -173,4 +170,40 @@ public class SelectAlbumActivity extends Activity implements AdapterView.OnItemC
         }
     }
 
+
+    private class EntryAdapter extends ArrayAdapter<MusicDirectory.Entry> {
+        public EntryAdapter(List<MusicDirectory.Entry> entries) {
+            super(SelectAlbumActivity.this, android.R.layout.simple_list_item_1, entries);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            MusicDirectory.Entry entry = getItem(position);
+            TextView view;
+
+            if (entry.isDirectory()) {
+                if (convertView != null && convertView instanceof TextView && !(convertView instanceof CheckedTextView)) {
+                    view = (TextView) convertView;
+                    Log.i(TAG, "Reusing album view.");
+                } else {
+                    view = (TextView) LayoutInflater.from(SelectAlbumActivity.this).inflate(
+                            android.R.layout.simple_list_item_1, parent, false);
+                    Log.i(TAG, "Creating new album view.");
+                }
+            } else {
+                if (convertView != null && convertView instanceof CheckedTextView) {
+                    view = (TextView) convertView;
+                    Log.i(TAG, "Reusing song view.");
+                } else {
+                    view = (TextView) LayoutInflater.from(SelectAlbumActivity.this).inflate(
+                            android.R.layout.simple_list_item_multiple_choice, parent, false);
+                    Log.i(TAG, "Creating new song view.");
+                }
+            }
+
+            view.setText(entry.getName());
+
+            return view;
+        }
+    }
 }
