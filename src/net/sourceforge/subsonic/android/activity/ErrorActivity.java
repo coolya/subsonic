173b13645cd32dfc78716c79f3cf92e9210125c2
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2009-08-18 16:57:59 +0000

    Android: Verify that server address always is a valid url.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk/subsonic-android@977 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- src/net/sourceforge/subsonic/android/activity/ErrorActivity.java
+++ src/net/sourceforge/subsonic/android/activity/ErrorActivity.java
@@ -23,6 +23,6 @@ public class ErrorActivity extends Activity {
         String errorMessage = getIntent().getStringExtra(Constants.INTENT_EXTRA_NAME_ERROR);
 
         Log.i(TAG, "ErrorActivity got message: " + errorMessage + ", " + System.identityHashCode(this));
-        new ErrorDialog(this, errorMessage);
+        new ErrorDialog(this, errorMessage, true);
     }
 }
