1afbd788e59b6fa163b25782f74e075acc14d04c
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2009-10-09 10:52:08 +0000

    Android: Show version number in Help.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk/subsonic-android@1094 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- src/net/sourceforge/subsonic/androidapp/activity/HelpActivity.java
+++ src/net/sourceforge/subsonic/androidapp/activity/HelpActivity.java
@@ -1,15 +1,20 @@
 package net.sourceforge.subsonic.androidapp.activity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.content.Intent;
-import android.net.Uri;
 import net.sourceforge.subsonic.androidapp.R;
 import net.sourceforge.subsonic.androidapp.util.Constants;
 
 public class HelpActivity extends OptionsMenuActivity {
+
+    private static final String TAG = HelpActivity.class.getSimpleName();
 
     /**
      * Called when the activity is first created.
@@ -20,7 +25,8 @@ public class HelpActivity extends OptionsMenuActivity {
         setContentView(R.layout.help);
         TextView helpTextView = (TextView) findViewById(R.id.help_text);
 
-        helpTextView.setText("With Subsonic you can easily stream or download music from your home computer to your Android phone " +
+        StringBuilder text = new StringBuilder();
+        text.append("With Subsonic you can easily stream or download music from your home computer to your Android phone " +
                 "(and do lots of other cool stuff too).\n" +
                 "\n" +
                 "To install the Subsonic server software on your computer, please visit http://subsonic.sf.net. It's available for " +
@@ -36,7 +42,19 @@ public class HelpActivity extends OptionsMenuActivity {
                 " o No ads in the Subsonic web interface.\n" +
                 " o Free access to new premium features.\n" +
                 "\n" +
-                "The suggested donation amount is \u20ac20.");
+                "The suggested donation amount is \u20ac20.\n\n");
+
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo("net.sourceforge.subsonic.androidapp", 0);
+            if (packageInfo != null) {
+                text.append("Application version: ").append(packageInfo.versionName).append("\n");
+            }
+        } catch (PackageManager.NameNotFoundException x) {
+            Log.w(TAG, "Failed to resolve application version name.", x);
+        }
+        text.append("REST API version: ").append(Constants.REST_PROTOCOL_VERSION).append("\n");
+
+        helpTextView.setText(text);
 
         Button okButton = (Button) findViewById(R.id.help_close);
         okButton.setOnClickListener(new View.OnClickListener() {
