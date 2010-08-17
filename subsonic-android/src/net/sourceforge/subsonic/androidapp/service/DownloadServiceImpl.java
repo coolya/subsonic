ac62396bc10d8c70e3dec6fb517da4e907292f87
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2010-08-17 16:12:16 +0000

    Android: Fix NPE in DownloadServiceImpl.handleError().
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@1747 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-android/src/net/sourceforge/subsonic/androidapp/service/DownloadServiceImpl.java
+++ subsonic-android/src/net/sourceforge/subsonic/androidapp/service/DownloadServiceImpl.java
@@ -421,9 +421,11 @@ public class DownloadServiceImpl extends Service implements DownloadService {
     }
 
     private void handleError(Exception x) {
-        String msg = getResources().getString(R.string.download_play_error, currentPlaying.getSong().getTitle());
-        Util.showErrorNotification(this, handler, msg, x);
-        Log.e(TAG, msg, x);
+        if (currentPlaying != null) {
+            String msg = getResources().getString(R.string.download_play_error, currentPlaying.getSong().getTitle());
+            Util.showErrorNotification(this, handler, msg, x);
+            Log.e(TAG, msg, x);
+        }
         mediaPlayer.reset();
         setPlayerState(IDLE);
     }
