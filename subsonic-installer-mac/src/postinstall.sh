9f7d02ae2acd84f18c4f75cae4024ad4f377b8f1
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2010-09-29 04:43:24 +0000

    Mac: Include statically linked ffmpeg, lame from einstein2x.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@1817 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-installer-mac/src/postinstall.sh
+++ subsonic-installer-mac/src/postinstall.sh
@@ -2,13 +2,8 @@
 
 SUBSONIC_HOME="/Library/Application Support/Subsonic"
 
-mkdir -p "$SUBSONIC_HOME/transcode"
 chmod oug+rwx "$SUBSONIC_HOME"
 chown root:admin "$SUBSONIC_HOME"
 rm -rf "$SUBSONIC_HOME/jetty"
-
-echo Creating transcoder symlinks
-[ ! -e "$SUBSONIC_HOME/transcode/lame" ] && ln -sf /opt/local/bin/lame "$SUBSONIC_HOME/transcode/"
-[ ! -e "$SUBSONIC_HOME/transcode/ffmpeg" ] && ln -sf /opt/local/bin/ffmpeg "$SUBSONIC_HOME/transcode/"
 
 echo Subsonic installation done
