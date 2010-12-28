fcff8f8b7640c158ee0271c3511b3e76d4d29aad
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2010-12-28 12:01:34 +0000

    Delete existing video transcodings in Schema50.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@2037 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-main/src/main/java/net/sourceforge/subsonic/dao/schema/Schema50.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/dao/schema/Schema50.java
@@ -42,6 +42,7 @@ public class Schema50 extends Schema {
             template.execute("insert into version values (16)");
 
             for (String format : Arrays.asList("avi", "mpg", "mpeg", "mp4", "m4v", "mkv", "mov", "wmv", "ogv")) {
+                template.update("delete from transcoding where source_format=? and target_format=?", new Object[] {format, "flv"});
                 template.execute("insert into transcoding values(null,'" + format + " > flv' ,'" + format + "' ,'flv','ffmpeg -ss %o -i %s -b %bk -s %wx%h -ar 44100 -ac 2 -v 0 -f flv -',null,null,true,true)");
                 template.execute("insert into player_transcoding select p.id as player_id, t.id as transaction_id from player p, transcoding t where t.name = '" + format + " > flv'");
             }
