becea910f3eb674822abd8235f9f84adf185f01b
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2011-01-02 20:47:17 +0000

    Android: New settings "Hide from other" and "Media buttons"
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk/subsonic-android@2050 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- src/net/sourceforge/subsonic/androidapp/util/Util.java
+++ src/net/sourceforge/subsonic/androidapp/util/Util.java
@@ -651,15 +651,23 @@ public final class Util {
     }
 
     public static void registerMediaButtonEventReceiver(Context context) {
-        // AudioManager.registerMediaButtonEventReceiver() was introduced in Android 2.2.
-        // Use reflection to maintain compatibility with 1.5.
-        try {
-            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-            ComponentName componentName = new ComponentName(context.getPackageName(), MediaButtonIntentReceiver.class.getName());
-            Method method = AudioManager.class.getMethod("registerMediaButtonEventReceiver", ComponentName.class);
-            method.invoke(audioManager, componentName);
-        } catch (Throwable x) {
-            // Ignored.
+
+        // Only do it if enabled in the settings.
+        SharedPreferences prefs = getPreferences(context);
+        boolean enabled = prefs.getBoolean(Constants.PREFERENCES_KEY_MEDIA_BUTTONS, true);
+
+        if (enabled) {
+
+            // AudioManager.registerMediaButtonEventReceiver() was introduced in Android 2.2.
+            // Use reflection to maintain compatibility with 1.5.
+            try {
+                AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                ComponentName componentName = new ComponentName(context.getPackageName(), MediaButtonIntentReceiver.class.getName());
+                Method method = AudioManager.class.getMethod("registerMediaButtonEventReceiver", ComponentName.class);
+                method.invoke(audioManager, componentName);
+            } catch (Throwable x) {
+                // Ignored.
+            }
         }
     }
 
