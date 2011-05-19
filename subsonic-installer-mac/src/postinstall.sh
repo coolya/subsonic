d03c6b8f8653fae316b4c1cca9c105751f168616
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2011-05-19 15:52:23 +0000

    Mac installer: make transcode dir executable.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@2211 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-installer-mac/src/postinstall.sh
+++ subsonic-installer-mac/src/postinstall.sh
@@ -4,6 +4,10 @@ SUBSONIC_HOME="/Library/Application Support/Subsonic"
 
 chmod oug+rwx "$SUBSONIC_HOME"
 chown root:admin "$SUBSONIC_HOME"
+
+chmod oug+rx "$SUBSONIC_HOME/transcode"
+chown root:admin "$SUBSONIC_HOME/transcode"
+
 rm -rf "$SUBSONIC_HOME/jetty"
 
 echo Subsonic installation done
