1e7ab0916f0150525e9e2ad1d3bf67a13916a693
sindre_mehus <sindre_mehus@24148959-8e0e-0410-9c20-a6ce11f09cd5>
2011-06-14 18:37:20 +0000

    Android: Show proper title in SearchActivity.
    git-svn-id: https://subsonic.svn.sourceforge.net/svnroot/subsonic/trunk@2274 24148959-8e0e-0410-9c20-a6ce11f09cd5

--- subsonic-android/src/net/sourceforge/subsonic/androidapp/activity/SearchActivity.java
+++ subsonic-android/src/net/sourceforge/subsonic/androidapp/activity/SearchActivity.java
@@ -97,8 +97,8 @@ public class SearchActivity extends SubsonicTabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
 
-        setTitle(R.string.common_appname);
-        
+        setTitle(R.string.search_title);
+
         View buttons = LayoutInflater.from(this).inflate(R.layout.search_buttons, null);
 
         artistsHeading = buttons.findViewById(R.id.search_artists);
@@ -142,7 +142,7 @@ public class SearchActivity extends SubsonicTabActivity {
             }
         });
         registerForContextMenu(list);
-		
+
 		// Button 1: gone
 		findViewById(R.id.action_button_1).setVisibility(View.GONE);
 
@@ -165,7 +165,7 @@ public class SearchActivity extends SubsonicTabActivity {
         String query = intent.getStringExtra(Constants.INTENT_EXTRA_NAME_QUERY);
         boolean autoplay = intent.getBooleanExtra(Constants.INTENT_EXTRA_NAME_AUTOPLAY, false);
         boolean requestsearch = intent.getBooleanExtra(Constants.INTENT_EXTRA_REQUEST_SEARCH, false);
-        
+
         if (query != null) {
             mergeAdapter = new MergeAdapter();
             list.setAdapter(mergeAdapter);
@@ -173,7 +173,8 @@ public class SearchActivity extends SubsonicTabActivity {
         } else {
             populateList();
             if (requestsearch)
-            	onSearchRequested();        }
+                onSearchRequested();
+        }
     }
 
     @Override
